41e7a1255f757aa787ad78cca5c37d876923a7c8
Eric Germain <eric.germain@hec.ca>
2019-06-17 18:26:09 -0400

    predict.R : improvements to determination of values for E in predict.traveltime; creation of function validateE; inclusion of E as parameter with default NULL; and removal of '...' in all interfaces.

--- R/predict.R
+++ R/predict.R
@@ -1,33 +1,62 @@
 
 #' @export
-predict.traveltime<-function(object, data, starttime = Sys.time(),  n=1000, ...){
-    if(!is.list(data))
-        stop('data must be a list, data.fram or data.table')
-    if(!all(c('linkId', 'length') %in% names(data)))
-        stop('data must have objects named linkId and length, corresponding order of travelled links and their lengths')
-    if(length(data$linkId)!=length(data$length))
-        stop('length of objects do not match!')
-    
-    if(object$model == 'no-dependence')
-        return(predict.traveltime.no_dependence(object, data , starttime, n, ...))
-    
-    if(object$model =='trip')
-        return(predict.traveltime.no_dependence(object, data , starttime, n, ...))
-    
-    if(grepl('HMM', object$model))
-        return(predict.traveltime.HMM(object, data, starttime, n, ...))
+
+# Function validateE returns the appropriate value for E.
+# E can be passed either as a separate parameter with default value NULL, 
+# or as part of 'object'.  Valid values of parameter E are considered first and override
+# any value for E in 'object'.  However, any value passed (either by parameter or
+# through 'object) need to be a vector of length 'n'; otherwise it is discarded
+# If no valid value is found, then we either assign a vector of random values following
+# a normal distribution with mean 0 and standard deviation tau (if trip model), or a
+# vector of zeros (otherwise).
+
+validateE <- function(object, E, n) {
+  # Part 1 of validation.  We check for the existence of parameter E.
+  if(is.null(E)) { # If not found, we assign value in 'object' if valid.
+    if('E' %in% names(object) && !is.null(object$E)) {
+      if(is.numeric(object$E) && length(object$E)==n) 
+        E <- object$E
+      else
+        message("Values E in object need to be a vector of length n.  Values for E are discarded.")
+    }
+  } else if(!is.numeric(E) || length(E)!=n) { # If a parameter exists, use it only if valid,
+                                              # otherwise set to NULL.
+    message("Values in parameter E need to be a vector of length n.  Values for E are discarded.")
+    E <- NULL
+  }
+  # At this point, E has the appropriate value among valid parameters passed,
+  # and NULL if no such value is found.
+  
+  # Part 2 of validation: if E is null...
+  
+  if(is.null(E)) {
+    if(grepl('trip', object$model)) 
+      E = rnorm(n, mean = 0, sd = object$tau) # ... we define a vector of random values if trip model...
+    else E = 0 # ... otherwise we set to zero.
+  }
+  E # We return the result.
 }
 
-predict.traveltime.no_dependence <- function(object, data, starttime, n = 1000, ...) {
-    param = list(...)
+
+predict.traveltime<-function(object, data, starttime = Sys.time(),  n = 1000, E = NULL){
+    if(!is.list(data))
+      stop('data must be a list, data.frame or data.table')
+    if(!all(c('linkId', 'length') %in% names(data)))
+      stop('data must have objects named linkId and length, corresponding order of travelled links and their lengths')
+    if(length(data$linkId)!=length(data$length))
+      stop('length of objects do not match!')
+  
+    if(grepl('HMM', object$model))
+      predict.traveltime.HMM(object, data, starttime, n, E)
+    else
+      predict.traveltime.no_dependence(object, data , starttime, n, E)
+}
+
+predict.traveltime.no_dependence <- function(object, data, starttime, n = 1000, E = NULL) {
     linkIds = data$linkId
     len = data$length
     ## sampling E (trip-effect)
-    if(!is.null(param$E) && is.numeric(param$E)){
-        E = param$E
-    }else if(grepl('trip', object$model))
-        E = rnorm(n, mean = 0, sd = object$tau) else E = 0
-    
+    E <- validateE(object, E, n)
     fact = paste(linkIds[1], time_bins(starttime), sep = ".")
     id = which(levels(object$factors) == fact)
     speed = rnorm(n, object$mean[id, ], object$sd[id, ])
@@ -43,15 +72,11 @@ predict.traveltime.no_dependence <- function(object, data, starttime, n = 1000, 
 }
 
 
-predict.traveltime.HMM <- function(object, data, starttime, n = 1000, ...) {
+predict.traveltime.HMM <- function(object, data, starttime, n = 1000, E = NULL) {
     ## sampling E (trip-effect)
-    param = list(...)
     linkIds = data$linkId
     len = data$length
-    if(!is.null(param$E) && is.numeric(param$E)){
-        E = param$E
-    }else if(grepl('trip', object$model))
-        E = rnorm(n, mean = 0, sd = object$tau) else E = 0
+    E <- validateE(object, E, n)
     nQ = object$nQ
     id = which(levels(object$factors) == paste(linkIds[1], time_bins(starttime), 
                          sep = "."), useNames = FALSE)
