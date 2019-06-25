9c08f39ba14e1a9ff73932819b5267e40b915d4a
Eric Germain <eric.germain@hec.ca>
2019-06-24 21:41:39 -0400

    Comments to misc.R

--- R/misc.R
+++ R/misc.R
@@ -1,6 +1,19 @@
+#' Miscellaneous functions for package traveltimeHMM
+#' 
+#' @name misc
+NULL
 
-
-# Normalize a matrix (m X n) by the sum of its elements
+#' @keywords internal
+#' Normalize a vector by the sum of its elements
+#' Function \code{normalizeV} normalizes a vector by the sum of its elements. It is much faster than sum or .colSums(x, m=1, n=length(x)) .
+#' @param x  A vector of dimension \code{n}. 
+#' @return A vector of dimension \code{n} with  its elements summing up to 1.
+#' @examples
+#' normalizeV(c(1,3))
+#' 
+#' \dontrun{
+#' normalizeV(matrix(c(1,2,3,4), nrow=2, ncol = 2))
+#' }
 normalizeV <- function(x) {
   if(class(x)=="numeric")
     x/.colSums(x, m = length(x), n = 1)  # a much faster version compared to sum or .colSums(x, m=1, n=length(x))
@@ -8,9 +21,22 @@ normalizeV <- function(x) {
     stop("Function normalizeV requires a vector.")
 }
 
-# Normalize a matrix (m X n) by rowSums
+#' @keywords internal
+#' Normalize each row of a matrix by the sum of its elements
+#' Function \code{normalizeR} normalizes each row of a matrix by the sum of its elements.
+#' @param x  A matrix of dimensions \code{m X n}.
+#' @param m The number of rows of the matrix.
+#' @param n The number of columns of the matrix.
+#' @return A matrix of dimensions \code{m X n} with each row summing up to 1.
+#' @examples
+#' normalizeR(matrix(c(1,2,3,4), nrow=2, ncol = 2))
+#' 
+#' \dontrun{
+#' normalizeV(matrix(c(1,2,3,4), nrow=2, ncol = 2))
+#' }# Normalize a matrix (m X n) by rowSums
 normalizeR <- function(x, m, n) x/.rowSums(x, m, n)  
 
+# To be documenter later (this function might be removed in the future).
 order_states <- function(param) {
     ## ordering of estimates parameters per state, to force an ordering in the HMM
     ## param : n x Q matrix, where Q is the number of states return : n x Q where for
