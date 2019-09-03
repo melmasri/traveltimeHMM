2d24251438a21df4866e40a8fbe5848d1c628048
Mohamad <mo@julia>
2019-09-02 21:43:25 -0700

    updated desc

--- R/gaussian_param_by_factor.R
+++ R/gaussian_param_by_factor.R
@@ -1,8 +1,8 @@
 #' Calculate the parameters of Gaussian speeds
-#' @keywords internal
+#' @keywords intaernal
 #' 
 #' \code{gaussian_param_by_factor} calculates the mean and variance of Gaussian speeds.  The function refers to the calculation
-#' of \eqn{\mu} and \eqn{\sigma^{2}} at step 4 of Algorithm 1 in Woodard et al., 2017
+#' of mu and sigma2 at step 4 of Algorithm 1 in Woodard et al., 2017
 #' 
 #' @param logspeeds A numeric vector of speed observations (in km/h) on the (natural) log-scale, of size \code{n = number of observations}.
 #' @param byFactor A vector of factors (link + time bin) of size \code{n}, i.e. one factor for each speed observation.
@@ -12,8 +12,6 @@
 #' @return \code{gaussian_param_by_factor} returns a list of two matrices of identical dimensions \code{m X nQ}
 #' where \code{m = number of levels} (links + timeBins).  The first list \code{mean} contains the vector of means
 #' for each level and state.  The second list \code{sigma2} contains the vector of variances for each level and state.  
-#' @references
-#' {Woodard, D., Nogin, G., Koch, P., Racz, D., Goldszmidt, M., Horvitz, E., 2017.  Predicting travel time reliability using mobile phone GPS data.  Transportation Research Part C, 75, 30-44.}
 #' @export
 gaussian_param_by_factor <- function(logspeeds, byFactor, states = NULL) {
     if (is.null(states)) { # If 'states' is not provided, we assume a single state '1' and proceed.
